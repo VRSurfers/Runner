5ab872c45e6c43beac1756fae8d5e2b110d8d3b5
AP <polishchuk.artyom@gmail.com>
2018-02-09 00:03:41 +0300

    Added: terrain + test person

---
+++ Assets/Prefabs/Runner/RunnerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RunnerController : MonoBehaviour {
+
+	class InputController
+	{
+		public static void Process(Transform transform)
+		{
+			if (Input.GetKey(KeyCode.A))
+			{
+				transform.position += -0.05f * transform.right;
+			}
+			else if (Input.GetKey(KeyCode.D))
+			{
+				transform.position += 0.05f * transform.right;
+			}
+		}
+	}
+
+	void Update ()
+	{
+		InputController.Process(transform);
+		transform.position += transform.forward * 0.1f;
+	}
+}
