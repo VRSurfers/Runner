a0266df944137aeadb3b4960b7b5780e5ff247d3
konlaw <konklaw@gmail.com>
2018-03-27 22:42:13 +0300

    added: targets

---
+++ Assets/Prefabs/DronFactory/DronFactory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DronFactory : MonoBehaviour {
+
+	public GameObject Target;
+	public DroneScript Dron;
+
+	private const float Interval = 1;
+	private float lastActivationTime;
+
+	// Use this for initialization
+	void Start () {
+		Dron.gameObject.SetActive(false);
+		Dron.Target = Target;
+		lastActivationTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > lastActivationTime + Interval)
+		{
+			Dron.transform.position = Target.transform.position + 20 * Target.transform.forward;
+			Dron.gameObject.SetActive(true);
+			lastActivationTime = Time.time + 1000;
+		}
+	}
+}
---
+++ Assets/Prefabs/SpaceDroidPackage/DroneScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneScript : MonoBehaviour {
+
+	public float HangingHeight = 2;
+	public float VerticalSpeed = 2;
+
+	public GameObject Target;
+		
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < HangingHeight)
+		{
+			transform.position += Time.deltaTime * new Vector3(0, VerticalSpeed, 0);
+		}
+
+		transform.up = Vector3.RotateTowards(
+			transform.up,
+			Target.transform.position - transform.position,
+			0.02f, 0f);
+	}
+}
