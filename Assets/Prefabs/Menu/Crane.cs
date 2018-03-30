f131b43ab0e1ddc7f6968df5253a4adaa15cc7a2
nkitsan <nastik.kitsan@gmail.com>
2018-03-30 23:13:06 +0300

    Added: menu

---
+++ Assets/Prefabs/Menu/Crane.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Crane : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+
+    public Renderer craneRenderer;
+    public Material darkerMaterial;
+    public string sceneName;
+    private Material mainMaterial;
+
+    void Start () {
+        mainMaterial = craneRenderer.material;
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        craneRenderer.material = darkerMaterial;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        craneRenderer.material = mainMaterial;
+    }
+}
