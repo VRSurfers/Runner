77c5a9688ba95945fd79594e7ed10216c01a80d4
Konklaw <konklaw@gmail.com>
2018-05-21 22:48:42 +0300

    refactored: motion controller

---
+++ Assets/Prefabs/Runner/KickTracker.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public partial class RunnerController
+{
+	class KickTracker
+    {
+		private const float FlyTime = 1.5f;
+		private const float AngleSpeed = 360 / FlyTime;
+		private const float Axeleration = 15f;
+		private const float LandingHeight = 0f;
+
+		public bool IsOnKick { get; private set; }
+
+		private readonly Transform transformWithCamera;
+		private readonly Transform transformOfModel;
+
+		private float targetX;
+		private float startTime;
+		private float horizontalSpeed;
+		private float verticalSpeed;
+
+		public KickTracker(Transform transformOfModel)
+		{
+			this.transformOfModel = transformOfModel;
+			transformWithCamera = transformOfModel.parent.transform;
+		}
+
+		public void KickTo(float targetX)
+		{
+			this.targetX = targetX;
+			startTime = Time.time;
+
+			horizontalSpeed = (targetX - transformWithCamera.position.x) / FlyTime;
+			verticalSpeed = Axeleration * FlyTime / 2f;
+			IsOnKick = true;
+		}
+
+        public void Step()
+        {
+			CommonHelpers.AssertIfTrue(IsOnKick);
+
+            Vector3 oldPosition = transformWithCamera.position;
+            float timePassed = (Time.time - startTime);
+            if (timePassed > FlyTime)
+			{
+				oldPosition.x += targetX - oldPosition.x;
+				oldPosition.y = LandingHeight;
+				transformOfModel.localRotation = Quaternion.Euler(0, 0, 0);
+				IsOnKick = false;
+			}
+			else
+			{
+				oldPosition.x += horizontalSpeed * Time.deltaTime;
+				oldPosition.y = timePassed * (verticalSpeed - Axeleration * timePassed / 2f);
+				transformOfModel.localRotation = Quaternion.Euler(AngleSpeed * timePassed, 0, 0);
+			}
+			transformWithCamera.position = oldPosition;
+		}
+    }
+}
--- Assets/Prefabs/Runner/RunnerController.cs
+++ Assets/Prefabs/Runner/RunnerController.cs
@@ -1,161 +1,109 @@
 ﻿using System;
 using UnityEngine;
 
-public class RunnerController : MonoBehaviour
+public partial class RunnerController : MonoBehaviour
 {
-    struct KickTracker
-    {
-        private const float FlyTime = 1.5f;
-        private readonly Transform transform;
-        private readonly float targetX;
-        float startTime;
-        private float G;
-        private float V0;
+	public Transform RunnerModel;
+	public MapController MapController;
+	public float MaxSideSpeed = 2;
 
-        private float dAngle;
-        private float horSpeedAbs;
-        private float horSpeedSign;
-
-        public KickTracker(Transform transform, float targetX)
-        {
-            this.transform = transform;
-            this.targetX = targetX;
-            startTime = Time.time;
-
-
-            horSpeedAbs = (targetX - transform.position.x) / FlyTime;
-            horSpeedSign = Math.Sign(horSpeedAbs);
-            horSpeedAbs *= horSpeedSign;
-
-            dAngle = (float) (360 / FlyTime);
-
-            G = 15f;
-            // V0 = g * t / 2;
-            V0 = G * FlyTime / 2f;
-        }
-
-        public bool Step()
-        {
-            Vector3 oldPos = transform.position;
-
-            float passed = (Time.time - startTime);
-
-            if (passed > FlyTime)
-                return true;
-
-            oldPos.x += Math.Min(horSpeedAbs * Time.deltaTime, (targetX - oldPos.x) * horSpeedSign) * horSpeedSign;
-
-            oldPos.y = Math.Max(V0 * passed - G * passed * passed / 2f, 0.077f);
-
-
-            transform.localRotation = Quaternion.Euler(dAngle * passed, 0, 0);
-            transform.parent.transform.position = oldPos;
-
-            return false;
-        }
-    }
-
-    public MapController MapController;
-    public float MaxSideSpeed = 2;
-
+	private KickTracker kickTracker;
     private float? sideSpeed;
+
+	private void Awake()
+	{
+		kickTracker = new KickTracker(RunnerModel);
+	}
 
 	void Update()
 	{
 		float? horizontal;
 		float? vertical;
 		InputController.GetmovementInfo(out horizontal, out vertical);
-		if (horizontal.HasValue && kickTracker == null)
+		if (horizontal.HasValue && !kickTracker.IsOnKick)
 		{
-            TryUpdateSideSpeed(horizontal.Value);
+            TryStartSideMotion(horizontal.Value);
 		}
 
-        if (kickTracker.HasValue)
-        {
-            if (kickTracker.Value.Step())
-                kickTracker = null;
-        }
+        if (kickTracker.IsOnKick)
+			kickTracker.Step();
         else
             MakeMoveAndTryStopSideMotion();
 	}
 
-    private void TryUpdateSideSpeed(float newSideSpeed)
+    private void TryStartSideMotion(float newSideSpeedSign)
     {
         if (sideSpeed.HasValue)
         {
-            sideSpeed = newSideSpeed;
+            sideSpeed = newSideSpeedSign * MaxSideSpeed;
             return;
         }
 
         // We are on track.
         float xCurrent = transform.position.x;
-        float relativePosition = (xCurrent - MapController.LeftRowX) / MapController.RowWidth; // teorically it should has int value (almost).
-        int trackNum = (int)Math.Round(relativePosition);
+        float relativePosition = (xCurrent - MapController.LeftRowX) / MapController.RowWidth;
+
+		// teorically it should has int value.
+		//CommonHelpers.AssertIfTrue(Math.Abs(relativePosition / 1f) > 0.001);
+
+        int trackNumber = (int)Math.Round(relativePosition);
         // check if we are on outside tracks
-        if (newSideSpeed < 0)
+        if (newSideSpeedSign < 0)
         {
-            if (trackNum == 0)
+            if (trackNumber == 0)
                 return;
         }
         else
         {
-            if (trackNum == MapController.RowCount - 1)
+            if (trackNumber == MapController.RowCount - 1)
                 return;
         }
-        sideSpeed = newSideSpeed;
+        sideSpeed = newSideSpeedSign * MaxSideSpeed;
     }
 
-    private KickTracker? kickTracker;
-    public Transform Capsule;
+	private void MakeMoveAndTryStopSideMotion()
+	{
+		if (sideSpeed.HasValue)
+		{
+			float leftRowX = MapController.LeftRowX;
+			float rowWidth = MapController.RowWidth;
 
+			float displacement;
+			Vector3 oldPositon = transform.position;
+			float xCurrent = oldPositon.x;
+			float maxDisplAbs = Time.deltaTime * MaxSideSpeed;
 
-    internal void FrontCollision(float x)
+			double xRelative = (xCurrent - leftRowX) / rowWidth;
+			if (Math.Abs(xRelative - Math.Round(xRelative)) < 0.00001) // we are starting motion
+			{
+				displacement = maxDisplAbs * Math.Sign(sideSpeed.Value);
+			}
+			else
+			{
+				float betweanTracksX = (xCurrent - leftRowX) % rowWidth;
+				float toTrackDist = sideSpeed.Value > 0 ? rowWidth - betweanTracksX : betweanTracksX; // >= 0
+				if (toTrackDist < maxDisplAbs)
+				{
+					displacement = toTrackDist * Math.Sign(sideSpeed.Value);
+					sideSpeed = null;
+				}
+				else
+				{
+					displacement = maxDisplAbs * Math.Sign(sideSpeed.Value);
+				}
+			}
+			transform.position = oldPositon + new Vector3(displacement, 0, 0);
+		}
+	}
+
+	internal void FrontCollision(float x)
     {
-        //var oldPos = transform.position;
-        //oldPos.x = x;
-        //transform.position = oldPos;
-
-        kickTracker = new KickTracker(Capsule, x);
+		kickTracker.KickTo(x);
         sideSpeed = null;
     }
 
     internal void SideCollision()
     {
         sideSpeed = -(sideSpeed.Value);
-    }
-
-    private void MakeMoveAndTryStopSideMotion()
-	{
-        if (sideSpeed.HasValue)
-        {
-            float leftRowX = MapController.LeftRowX;
-            float rowWidth = MapController.RowWidth;
-
-            float displacement;
-            Vector3 oldPositon = transform.position;
-            float xCurrent = oldPositon.x;
-            float maxDisplAbs = Time.deltaTime * MaxSideSpeed;
-
-            double xRelative = (xCurrent - leftRowX) / rowWidth;
-            if (Math.Abs(xRelative - Math.Round(xRelative)) < 0.00001) // we are starting motion
-            {
-                displacement = maxDisplAbs * Math.Sign(sideSpeed.Value);
-            }
-            else
-            {
-                float betweanTracksX = (xCurrent - leftRowX) % rowWidth;
-                float toTrackDist = sideSpeed.Value > 0 ? rowWidth - betweanTracksX : betweanTracksX; // >= 0
-                if (toTrackDist < maxDisplAbs)
-                {
-                    displacement = toTrackDist * Math.Sign(sideSpeed.Value);
-                    sideSpeed = null;
-                }
-                else
-                {
-                    displacement = maxDisplAbs * Math.Sign(sideSpeed.Value);
-                }
-            }
-            transform.position = oldPositon + new Vector3(displacement, 0, 0);
-        }
     }
 }
