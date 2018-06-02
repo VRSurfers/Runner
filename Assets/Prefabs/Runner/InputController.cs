114b514f58fa2e3e9f4e44a00fa35a7a26ad0b33
Konklaw <konklaw@gmail.com>
2018-06-02 12:47:48 +0300

    added: input controller added: image

--- Assets/Prefabs/Runner/InputController.cs
+++ Assets/Prefabs/Runner/InputController.cs
@@ -4,6 +4,15 @@ class InputController
 {
 	public static void GetmovementInfo(out float? horizontal, out float? vertical)
 	{
+#if UNITY_EDITOR
+		PCInput(out horizontal, out vertical);
+#elif UNITY_IOS || UNITY_ANDROID
+		MobileInput(out horizontal, out vertical);
+#endif
+	}
+
+	private static void PCInput(out float? horizontal, out float? vertical)
+	{
 		horizontal = null;
 		if (Input.GetKey(KeyCode.A))
 		{
@@ -23,5 +32,43 @@ class InputController
 		{
 			vertical = +1f;
 		}
+	}
+
+	private static Vector2 fingerDown;
+	private static Vector2 fingerUp;
+	public float SWIPE_THRESHOLD = 20f;
+
+	private static void MobileInput(out float? horizontal, out float? vertical)
+	{
+		foreach (Touch touch in Input.touches)
+		{
+			if (touch.phase == TouchPhase.Began)
+			{
+				fingerDown = touch.position;
+			}
+			if (touch.phase == TouchPhase.Ended)
+			{
+				fingerUp = touch.position;
+
+				Vector2 delta = fingerUp - fingerDown;
+				if (delta.magnitude > 20)
+				{
+					if (delta.x > 15)
+					{
+						horizontal = Mathf.Sign(delta.x);
+						vertical = null;
+						return;
+					}
+					else if (delta.y > 15)
+					{
+						horizontal = null;
+						vertical = Mathf.Sign(delta.y);
+						return;
+					}
+				}
+			}
+		}
+		horizontal = null;
+		vertical = null;
 	}
 }
